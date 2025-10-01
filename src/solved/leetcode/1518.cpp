80d6454cb1f4d2f62bb9d0867ea7ee3c79292464
Azruine <tyriba967@gmail.com>
2025-10-01 01:08:28 +0000

    데일리 문제 풀이 20251001

---
+++ src/solved/leetcode/1518.cpp
@@ -0,0 +1,45 @@
+/*==== Copyright 2025 Azruine ====*/
+
+#include <ranges>
+#include <utility>
+
+namespace {
+template <typename To, typename From>
+constexpr To as(From&& from) {
+    return static_cast<To>(std::forward<From>(from));
+}
+struct Range {
+    template <typename Sentinel>
+    constexpr auto operator()(Sentinel sentinel) const noexcept {
+        return std::views::iota(static_cast<Sentinel>(0), sentinel);
+    }
+    template <typename Start, typename Sentinel>
+    constexpr auto operator()(Start start, Sentinel sentinel) const noexcept {
+        return std::views::iota(start, sentinel);
+    }
+    template <typename Start, typename Sentinel, typename Step>
+    constexpr auto operator()(Start start, Sentinel sentinel,
+                              Step step) const noexcept {
+        return std::views::iota(start, sentinel) | std::views::stride(step);
+    }
+};
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+constexpr Range _iota{};
+#pragma GCC diagnostic pop
+}  // namespace
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int32_t ret = 0;
+        int remain = 0;
+        while (numBottles > 0) {
+            ret += numBottles;
+            int32_t temp = numBottles;
+            numBottles = (temp + remain) / numExchange;
+            remain = (temp + remain) % numExchange;
+        }
+        return ret;
+    }
+};

fb6e5bbfcb513b1659af853062dd42967fcd1769
inki-hwang <hik@enclony.com>
2026-01-12 01:10:44 +0000

    이상한 문제 풀기

---
+++ src/solved/baekjoon/24901.cpp
@@ -0,0 +1,53 @@
+//; # \u000a /\u002a
+#if true /*
+=begin
+/* /* */
+//\u000a/*
+#include <iostream>
+#define public
+#define static
+#define void int
+struct {
+    std::ostream& println() {
+        char const* ar[] = {"0"};
+        int x;
+        std::cin >> x;
+        return std::cout << ar[x];
+    }
+} out;
+//*/
+/*\u002a/
+import java.util.Scanner;
+class out {
+    static void println() {
+        String[] ar={"0"};
+        System.out.print(ar[new Scanner(System.in).nextInt()]);
+    }
+}
+public class Main {
+//*/
+public
+static void main(/*\u002a/String[] args//*/
+) {
+    out.println();
+}
+/*\u002a/
+}
+// */
+// */
+// */
+// */
+// */
+// */
+// */
+// */
+// */ print(["0"][Int(readLine()!)!])
+//; # \u000a /\u002a
+#if true /*
+=end
+puts %w(0)[gets.to_i]
+#*/
+//; # \u000a /\u002a
+#endif
+#endif
+//; # \u000a \u002a/

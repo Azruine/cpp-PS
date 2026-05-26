bdbfef70a2d64949f0d2fb40edbc97a484b6bc98
Azruine <tyriba967@gmail.com>
2026-05-26 02:14:57 +0000

    문제 풀이

---
+++ src/solved/leetcode/3120.cpp
@@ -0,0 +1,44 @@
+/*==== Copyright 2025 Azruine ====*/
+
+#include <ranges>
+#include <utility>
+
+namespace {
+template <typename To, typename From>
+constexpr To as(From&& from) {
+    return static_cast<To>(std::forward<From>(from));
+}
+struct Range {
+    template <typename Sentinel>
+    constexpr auto operator()(Sentinel sentinel) const noexcept {
+        return std::views::iota(static_cast<Sentinel>(0), sentinel);
+    }
+    template <typename Start, typename Sentinel>
+    constexpr auto operator()(Start start, Sentinel sentinel) const noexcept {
+        return std::views::iota(start, sentinel);
+    }
+    template <typename Start, typename Sentinel, typename Step>
+    constexpr auto operator()(Start start, Sentinel sentinel,
+                              Step step) const noexcept {
+        return std::views::iota(start, sentinel) | std::views::stride(step);
+    }
+};
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+constexpr Range _iota{};
+#pragma GCC diagnostic pop
+}  // namespace
+
+#include <cstdint>
+#include <string>
+
+class Solution {
+public:
+    int32_t numberOfSpecialChars(std::string word) {
+        uint64_t mask = 0;
+        for (auto&& chr : word) {
+            mask |= 1UL << (chr - 'A');
+        }
+        return std::popcount(mask & (mask >> 32) & ((1ULL << 26) - 1));
+    }
+};
